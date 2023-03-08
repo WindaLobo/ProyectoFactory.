917ee0255f7e64404a4e62745dcc8ca69afd199e
lobo2 <wlobogarces@danielcastelao.org>
2023-03-08 16:15:22 +0100

    Creando Readme

--- src/app.java
+++ src/app.java
@@ -17,12 +17,27 @@ public class app {
         System.out.println("Implementation Biclicleta");
         Transporte = FactorySencilla.getProducto(TipoTransporte.Bicicleta);
         System.out.println(Transporte.costoTotal(36470));
+        /**
+         * Con evalue Un tipo enumerado en Java es un tipo “especial” que en cierta medida puede usarse
+         * como una clase y admite ciertas posibilidades especiales.
+         */
         System.out.println(TipoEmbalaje.values()[Transporte.tipoEmbalajae(36470,2f,3f,5f,6f)]);
+        /**
+         * Sin values
+         */
+        System.out.println(Transporte.tipoEmbalajae(36470,2f,3f,5f,6f));
 
         System.out.println("Implementation Camion");
         Transporte = FactorySencilla.getProducto(TipoTransporte.Camion);
         System.out.println(Transporte.costoTotal(36470));
+        /**
+         * Con evalue Un tipo enumerado en Java es un tipo “especial” que en cierta medida puede usarse
+         * como una clase y admite ciertas posibilidades especiales.
+         */
         System.out.println(TipoEmbalaje.values()[Transporte.tipoEmbalajae(36470,2f,3f,5f,6f)]);
-
+        /**
+         * Sin values
+         */
+        System.out.println(Transporte.tipoEmbalajae(36470,2f,3f,5f,6f));
     }
 }
